b1f48ef01a03f0e846c2c2f03ac851a9d528aab2
Asmibug <12985693+asmibug@users.noreply.github.com>
2023-09-06 18:41:38 +0200

    task 1199

---
+++ 1199_minimum_time_to_build_blocks/main.cpp
@@ -0,0 +1,145 @@
+#include <algorithm>  // std::max std::max_element std::min std::min_element
+    // std::sort std::swap
+#include <climits>  // INT_MAX
+#include <functional>  // std::greater
+#include <queue>  // std::priority_queue
+#include <vector>  // std::vector
+
+namespace {
+    int MinBuildTimeRecrusive(const std::vector<int>& blocks,
+                              int split,
+                              int blocks_left,
+                              int workers,
+                              std::vector<std::vector<int>>& memo) {
+        if (blocks_left == 0) {
+            return 0;
+        }
+        if (workers == 0) {
+            return INT_MAX;
+        }
+        workers = std::min(blocks_left, workers);
+        if (blocks_left == workers) {
+            return blocks[blocks_left - 1];
+        }
+        if (memo[blocks_left][workers]) {
+            return memo[blocks_left][workers];
+        }
+        int t_build = std::max(blocks[blocks_left - 1], MinBuildTimeRecrusive(
+            blocks, split, blocks_left - 1, workers - 1, memo));
+        int t_split = split + MinBuildTimeRecrusive(
+            blocks, split, blocks_left, 2 * workers, memo);
+        int time = std::min(t_build, t_split);
+        memo[blocks_left][workers] = time;
+        return time;
+    }
+}
+
+int MinBuildTimeWithRecursion(std::vector<int> blocks, int split) {
+    std::sort(blocks.begin(), blocks.end());
+    std::vector<std::vector<int>> memo(blocks.size() + 1,
+                                       std::vector<int>(blocks.size() + 1));
+    return MinBuildTimeRecrusive(blocks, split, blocks.size(), 1, memo);
+}
+
+int MinBuildTimeDynamic(std::vector<int> blocks, int split) {
+    std::sort(blocks.begin(), blocks.end());
+    std::vector<int> time_for_workers_num(blocks.size() + 1);
+    for (int blocks_num = 1; blocks_num <= blocks.size(); ++blocks_num) {
+        std::vector<int> new_times(blocks.size() + 1);
+        new_times[0] = INT_MAX;
+        new_times[blocks_num] = blocks[blocks_num - 1];
+        for (int workers_num = blocks_num - 1; workers_num > 0; --workers_num) {
+            int time_if_work = std::max(blocks[blocks_num - 1],
+                                        time_for_workers_num[workers_num - 1]);
+            int workers_if_split = std::min(blocks_num, 2 * workers_num);
+            int time_if_split = split + new_times[workers_if_split];
+            new_times[workers_num] = std::min(time_if_work, time_if_split);
+        }
+        std::swap(time_for_workers_num, new_times);
+    }
+    return time_for_workers_num[1];
+}
+
+int MinBuildTimePriority(const std::vector<int>& blocks, int split) {
+    // min-queue
+    std::priority_queue merged_blocks(blocks.begin(), blocks.end(),
+                                      std::greater<int>());
+    while (merged_blocks.size() > 1) {
+        int first = merged_blocks.top();
+        merged_blocks.pop();
+        int second = merged_blocks.top();
+        merged_blocks.pop();
+        merged_blocks.push(split + std::max(first, second));
+    }
+    return merged_blocks.top();
+}
+
+using std::vector;
+
+class Solution {
+public:
+    int minBuildTime(vector<int>& blocks, int split) {
+        return MinBuildTimePriority(blocks, split);
+    }
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+#include "acutest.h"  // TEST_CHECK TEST_LIST TEST_MSG
+
+#define CHECK_MinBuildTime(func, expected, blocks, split) do { \
+    int t; \
+    TEST_CHECK(expected == (t = func(blocks, split))); \
+    TEST_MSG("Function returned %d", t); \
+} while (0)
+
+void TestOne() {
+    CHECK_MinBuildTime(MinBuildTimeWithRecursion, 7, std::vector<int>{7}, 1);
+}
+
+void TestTwo() {
+    CHECK_MinBuildTime(MinBuildTimeWithRecursion, 8,
+                       (std::vector<int>{6, 7}), 1);
+}
+
+void TestThree() {
+    CHECK_MinBuildTime(MinBuildTimeWithRecursion, 8,
+                       (std::vector<int>{5, 6, 7}), 1);
+}
+
+void TestOneDynamic() {
+    CHECK_MinBuildTime(MinBuildTimeDynamic, 7, std::vector<int>{7}, 1);
+}
+
+void TestTwoDynamic() {
+    CHECK_MinBuildTime(MinBuildTimeDynamic, 8, (std::vector<int>{6, 7}), 1);
+}
+
+void TestThreeDynamic() {
+    CHECK_MinBuildTime(MinBuildTimeDynamic, 8, (std::vector<int>{5, 6, 7}), 1);
+}
+
+void TestOnePriority() {
+    CHECK_MinBuildTime(MinBuildTimePriority, 7, std::vector<int>{7}, 1);
+}
+
+void TestTwoPriority() {
+    CHECK_MinBuildTime(MinBuildTimePriority, 8, (std::vector<int>{6, 7}), 1);
+}
+
+void TestThreePriority() {
+    CHECK_MinBuildTime(MinBuildTimePriority, 8, (std::vector<int>{5, 6, 7}), 1);
+}
+
+TEST_LIST = {
+    {"test one block", TestOne},
+    {"test two blocks", TestTwo},
+    {"test three blocks", TestThree},
+    {"test one block, dynamic algorithm", TestOneDynamic},
+    {"test two blocks, dynamic algorithm", TestTwoDynamic},
+    {"test three blocks, dynamic algorithm", TestThreeDynamic},
+    {"test one block, priority algorithm", TestOnePriority},
+    {"test two blocks, priority algorithm", TestOnePriority},
+    {"test three blocks, priority algorithm", TestOnePriority},
+    {NULL, NULL}
+};
